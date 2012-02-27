67a1eb1e1ff14d6d43be3166bc8217ebdf7037ed
Kevin Stanton <kevin@sproutsocial.com>
2012-02-27 17:26:57 -0600

    fixing pip issues still, bump to 0.1.3 to upload new dist

--- setup.py
+++ setup.py
@@ -1,18 +1,13 @@
 from distutils.core import setup
-import shutil, os
-
-shutil.copy('README.md', 'README')
 
 setup(
     name='pyfaceb',
-    version='0.1.2',
+    version='0.1.3',
     author='Kevin Stanton',
     author_email='kevin@sproutsocial.com',
     packages=['pyfaceb', 'pyfaceb.test'],
     url='https://bitbucket.org/sproutsocial/pyfaceb',
     license='LICENSE.txt',
     description='Full-featured, lightweight Facebook API wrapper for Graph & FQL.',
-    long_description=open('README').read(),
+    long_description=open('README.txt').read(),
 )
-
-os.remove('README')
